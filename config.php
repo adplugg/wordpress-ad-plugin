fc1d7be352023879336a89366acb12a13eb1b2db
adplugg <adplugg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-05 21:55:14 +0000

    Zone support
    git-svn-id: http://plugins.svn.wordpress.org/adplugg/trunk@852253 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Optional Configuration file. This file is not part of the repository and is
+ * used to override certain constants. This allows us to customize parts of the
+ * plugin for the test environment.
+ * @package AdPlugg
+ * @since 1.1
+ */
+
+//Override the ADPLUGG_HOST
+define('ADPLUGG_HOST', '192.168.33.10/apusers/app_dev.php');
+
+//Override the ADPLUGG_VERSION
+//define('ADPLUGG_VERSION', '1.1');
