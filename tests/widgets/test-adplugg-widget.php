7c2d92486e93771ef25233df9c098679f2d721b5
adplugg <adplugg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-04 16:21:59 +0000

    Added support for widget titles.
    git-svn-id: http://plugins.svn.wordpress.org/adplugg/trunk@1019605 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tests/widgets/test-adplugg-widget.php
+++ tests/widgets/test-adplugg-widget.php
@@ -30,25 +30,32 @@ class AdPlugg_Widget_Test extends WP_UnitTestCase {
         $adplugg_widget->form($instance);
         $outbound = ob_get_contents();
         ob_end_clean();
-        $this->assertContains('adplugg-fieldset', $outbound);
+        $this->assertContains('adplugg-widget-fieldset', $outbound);
     }
     
     /**
      * Test the update function.
      */    
     public function test_update() {
+        $old_title = 'old_title';
+        $new_title = 'new_title';
         $old_zone = 'old_zone';
         $new_zone = 'new_zone';
         $adplugg_widget = new AdPlugg_Widget();
         
         $old_instance = array();
+        $old_instance['title'] = $old_title;
         $old_instance['zone'] = $old_zone;
         
         $new_instance = array();
+        $new_instance['title'] = $new_title;
         $new_instance['zone'] = $new_zone;
         
         //Run the function.
         $ret_instance = $adplugg_widget->update($new_instance, $old_instance);
+        
+        //Assert that the ret_instance title is the new title
+        $this->assertEquals($ret_instance['title'], $new_title);
         
         //Assert that the ret_instance zone is the new zone
         $this->assertEquals($ret_instance['zone'], $new_zone);
@@ -71,7 +78,48 @@ class AdPlugg_Widget_Test extends WP_UnitTestCase {
         $outbound = ob_get_contents();
         ob_end_clean();
         $this->assertContains('adplugg-tag', $outbound);
+    }
+    
+    /**
+     * Test the widget function outputs zone info (when set).
+     */    
+    public function test_widget_zones() {
+        //Set up the variables.
+        $adplugg_widget = new AdPlugg_Widget();
+        $args = array();
+        $args['before_widget'] = '';
+        $args['after_widget'] = '';
+        $instance = array();
+        $instance['zone'] = 'test_zone';
         
+        //Assert that the widget form is output with zone info.
+        ob_start();
+        $adplugg_widget->widget($args, $instance);
+        $outbound = ob_get_contents();
+        ob_end_clean();
+        $this->assertContains('data-adplugg-zone="test_zone"', $outbound);
+    }
+    
+    /**
+     * Test the widget function outputs title (when set).
+     */    
+    public function test_widget_title() {
+        //Set up the variables.
+        $adplugg_widget = new AdPlugg_Widget();
+        $args = array();
+        $args['before_widget'] = '';
+        $args['after_widget'] = '';
+        $args['before_title'] = '';
+        $args['after_title'] = '';
+        $instance = array();
+        $instance['title'] = 'test_title';
+        
+        //Assert that the widget form is output with a title.
+        ob_start();
+        $adplugg_widget->widget($args, $instance);
+        $outbound = ob_get_contents();
+        ob_end_clean();
+        $this->assertContains('test_title', $outbound);
     }
     
 }
