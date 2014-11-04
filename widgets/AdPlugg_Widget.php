9ce95a2bbde49a90b64091e1c1b48d9d5847df00
Collin Krawll <collin.krawll@thirdset.com>
2014-11-04 08:01:41 -0800

    Added support for widget titles.

--- widgets/AdPlugg_Widget.php
+++ widgets/AdPlugg_Widget.php
@@ -26,20 +26,23 @@ class AdPlugg_Widget extends WP_Widget {
      */
     function form($instance) {
         // Check values
-	if( $instance) {
-	     $zone = esc_attr($instance['zone']);
-        } else {
-	     $zone = '';
-	}
+	$title = (($instance) && (isset($instance['title']))) 
+                    ? $instance['title'] : '';
+        $zone = (($instance) && (isset($instance['zone']))) 
+                    ? $instance['zone'] : '';
         
+        //Render the form
         echo '<p>
                   <a href="http://www.adplugg.com" target="_blank" title="Configure at adplugg.com">Configure at adplugg.com</a>
               </p>
               <fieldset class="adplugg-widget-fieldset">
                   <legend>Optional Settings</legend>
+                  <label for="' . $this->get_field_id('title') .'">Title:</label>
+                  <input class="widefat" id="' . $this->get_field_id('title') . '" name="' . $this->get_field_name('title') . '" type="text" value="' . $title . '" />
+                  <small>Enter a title for the widget.</small><br/>
                   <label for="' . $this->get_field_id('zone') .'">Zone:</label>
                   <input class="widefat" id="' . $this->get_field_id('zone') . '" name="' . $this->get_field_name('zone') . '" type="text" value="' . $zone . '" />
-                  <small>Enter the zone machine name.</small>
+                  <small>Enter the zone machine name.</small><br/>
               </fieldset>';
     }
 
@@ -48,6 +51,7 @@ class AdPlugg_Widget extends WP_Widget {
      */
     function update($new_instance, $old_instance) {
         $instance = $old_instance;
+        $instance['title'] = strip_tags($new_instance['title']);
         $instance['zone'] = strip_tags($new_instance['zone']);
         
         return $instance;
@@ -58,12 +62,22 @@ class AdPlugg_Widget extends WP_Widget {
      */
     function widget($args, $instance) {
         extract($args);
-        $zone = $instance['zone'];
+        //------ Set up the variables ---//
+        $title = (isset($instance['title'])) 
+                    ? apply_filters('widget_title', $instance['title'])
+                    : null;
+        $zone = (isset($instance['zone'])) ? $instance['zone'] : null;
         $zone_attribute = ($zone) ? ' data-adplugg-zone="' . $zone . '"' : '';
         
+        //------ Render the Widget ------//
         echo $before_widget;
+        
+        // Render the title (if there is one).
+        if ( $title ) {
+            echo $before_title . $title . $after_title;
+        }
 
-        // Display the widget
+        // Add the AdPlugg tag
         echo '<div class="adplugg-tag"'. $zone_attribute.'></div>';
 
         echo $after_widget;
