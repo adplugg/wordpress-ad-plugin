b197669a4adcb1e2acd06e732feb6ffc530c154b
Collin Krawll <collin.krawll@thirdset.com>
2014-11-19 07:31:49 -0800

    Added help link to options page

--- admin/pages/class-options-page.php
+++ admin/pages/class-options-page.php
@@ -38,6 +38,7 @@ class AdPlugg_Options_Page {
                 <ul>
                     <li>Manage my ads at <a href="https://www.adplugg.com/apusers/login" target="_blank">adplugg.com</a>.</li>
                     <li>Place the AdPlugg Widget on my site from the <a href="<?php echo admin_url( 'widgets.php' )?>">WordPress Widgets Configuration Page</a>.</li>
+                    <li>Get <a href="#" onclick="jQuery('a#contextual-help-link').trigger('click');">help</a> using this plugin.</li>
                 </ul>
             <?php } //end if ?>
         </div>
