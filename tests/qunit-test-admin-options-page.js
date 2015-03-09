eba726a0877513d8eaf7ba7bc93cc8aa27b55713
Collin Krawll <collin.krawll@thirdset.com>
2015-03-09 16:12:50 -0700

    Added QUnit tests for the admin options page

--- tests/qunit-test-admin-options-page.js
+++ tests/qunit-test-admin-options-page.js
@@ -6,4 +6,20 @@
  */
 
 
+/**
+* Assert that the page was rendered (by checking for the header).
+*/
+QUnit.test( "Test that the page was rendered", function( assert ) {
+    var header = $("#wpbody-content .wrap h2").html();
+    assert.equal(header, "AdPlugg Settings", "Settings page rendered");
+});
+
+/**
+* Assert that the help is rendered.
+*/
+QUnit.test( "Test that the help was rendered", function( assert ) {
+    var header = $("#tab-panel-adplugg_overview h1").html();
+    assert.equal(header, "AdPlugg Plugin Help", "Help rendered");
+});
+
 
