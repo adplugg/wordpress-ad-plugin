2ed915af1dc097f56692190f13afae942ddae87f
collinkrawll <collin.krawll@thirdset.com>
2016-06-14 10:33:57 -0700

    Updated test-widgets-page-help.php for WP coding standards.

--- tests/admin/help/test-widgets-page-help.php
+++ tests/admin/help/test-widgets-page-help.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(ADPLUGG_PATH . 'admin/help/widgets-page-help.php');
+require_once( ADPLUGG_PATH . 'admin/help/widgets-page-help.php' );
 
 /**
  * The WidgetsPageHelpTest class includes tests for testing the functions in the
@@ -16,15 +16,15 @@ class WidgetsPageHelpTest extends WP_UnitTestCase {
      */
     public function test_adplugg_widgets_page_help() {
         //set up the variables
-        $contextual_help = "";
+        $contextual_help = '';
         $screen_id = 'widgets';
-        $screen = WP_Screen::get($screen_id);
+        $screen = WP_Screen::get( $screen_id );
         
         //run the function
-        adplugg_widgets_page_help($contextual_help, $screen_id, $screen);
+        adplugg_widgets_page_help( $contextual_help, $screen_id, $screen );
         
         //Asset that the AdPlugg help is now in the screen.
-        $this->assertContains('AdPlugg Widget Help', serialize($screen));
+        $this->assertContains( 'AdPlugg Widget Help', serialize( $screen ) );
     }
     
 }
