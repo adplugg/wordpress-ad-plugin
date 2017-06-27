3a11f1d27415ab0f034f95e42862b4f40711eca8
Collin Krawll <collin.krawll@thirdset.com>
2017-06-27 12:51:18 -0700

    Updated test-functions.php for WP coding standards.

--- tests/includes/test-functions.php
+++ tests/includes/test-functions.php
@@ -15,14 +15,14 @@ class Test_Functions extends WP_UnitTestCase {
     function test_adplugg_is_access_code_installed() {
         //Clear out any options
         $options = array();
-        update_option(ADPLUGG_OPTIONS_NAME, $options);
+        update_option( ADPLUGG_OPTIONS_NAME, $options );
         
         //Assert function correctly determines access_code not installed
         $this->assertFalse( adplugg_is_access_code_installed() );
 
         //Install the access_code
         $options['access_code'] = 'test';
-        update_option(ADPLUGG_OPTIONS_NAME, $options);
+        update_option( ADPLUGG_OPTIONS_NAME, $options );
 
         //Assert function correctly determines access_code installed
         $this->assertTrue( adplugg_is_access_code_installed() );
@@ -35,16 +35,16 @@ class Test_Functions extends WP_UnitTestCase {
     function test_adplugg_get_active_access_code() {
         //Install the access_code
         $options = array();
-        update_option(ADPLUGG_OPTIONS_NAME, $options);
+        update_option( ADPLUGG_OPTIONS_NAME, $options );
         $options['access_code'] = 'test';
-        update_option(ADPLUGG_OPTIONS_NAME, $options);
+        update_option( ADPLUGG_OPTIONS_NAME, $options );
         
         $access_code = adplugg_get_active_access_code();
 
-        if(defined('ADPLUGG_OVERRIDE_ACCESS_CODE')) {
-            $this->assertContains(ADPLUGG_OVERRIDE_ACCESS_CODE, $access_code);
+        if( defined( 'ADPLUGG_OVERRIDE_ACCESS_CODE' ) ) {
+            $this->assertContains( ADPLUGG_OVERRIDE_ACCESS_CODE, $access_code );
         } else {
-            $this->assertEquals('test', $access_code);
+            $this->assertEquals( 'test', $access_code );
         }
     }
     
@@ -55,7 +55,7 @@ class Test_Functions extends WP_UnitTestCase {
     function test_adplugg_is_widget_active() {
         //Clear out any options
         $options = array();
-        update_option(ADPLUGG_WIDGET_OPTIONS_NAME, $options);
+        update_option( ADPLUGG_WIDGET_OPTIONS_NAME, $options );
         
         //Assert function correctly determines widget is not active
         $this->assertFalse( adplugg_is_widget_active() );
@@ -63,7 +63,7 @@ class Test_Functions extends WP_UnitTestCase {
         //Activate the widget
         $options[2]['zone'] = '';
         $options['_multiwidget'] = 1;
-        update_option(ADPLUGG_WIDGET_OPTIONS_NAME, $options);
+        update_option( ADPLUGG_WIDGET_OPTIONS_NAME, $options );
 
         //Assert function correctly determines widget is active
         $this->assertTrue( adplugg_is_widget_active() );
