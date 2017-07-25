7585a9d326a82aa8cc7496470ec2a0400e03c900
Collin Krawll <collin.krawll@thirdset.com>
2017-07-25 09:45:16 -0700

    Updated bootstrap.php for WP coding standards.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,13 +5,13 @@
  * @since 1.1.16
  */
 
-$_tests_dir = getenv('WP_TESTS_DIR');
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../adplugg.php';
+    require dirname( __FILE__ ) . '/../adplugg.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
