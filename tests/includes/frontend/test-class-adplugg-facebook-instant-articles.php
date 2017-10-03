076ff589c32425c382992d1c38f5c93448761fb3
Collin Krawll <collin.krawll@thirdset.com>
2017-10-03 09:04:14 -0700

    Added a unit test for the AdPlugg_Facebook_Instant_Articles->head_injector method.

--- tests/includes/frontend/test-class-adplugg-facebook-instant-articles.php
+++ tests/includes/frontend/test-class-adplugg-facebook-instant-articles.php
@@ -16,10 +16,36 @@ class Test_AdPlugg_Facebook_Instant_Articles extends WP_UnitTestCase {
      */    
     public function test_get_instance() {
         //get the singleton instance
-        $fbia = AdPlugg_Facebook_Instant_Articles::get_instance();
+        $adplugg_fbia = AdPlugg_Facebook_Instant_Articles::get_instance();
         
         //assert that the instance is returned and is the expected class.
-        $this->assertEquals( 'AdPlugg_Facebook_Instant_Articles', get_class( $fbia ) );
+        $this->assertEquals( 'AdPlugg_Facebook_Instant_Articles', get_class( $adplugg_fbia ) );
+    }
+    
+    /**
+     * Test the head_injector function.
+     */    
+    public function test_head_injector() {
+        //get the singleton instance
+        $adplugg_fbia = AdPlugg_Facebook_Instant_Articles::get_instance();
+        
+        //Enable the AdPlugg IA placement option
+        $options = array(
+                        'ia_enable_automatic_placement' => 1
+                    );
+        update_option( ADPLUGG_FACEBOOK_OPTIONS_NAME, $options );
+        
+        //mock some data
+        $ia_post = new \stdClass();
+        
+        //call the function
+        ob_start();
+        $adplugg_fbia->head_injector( $ia_post );
+        $output = ob_get_contents();
+        ob_end_clean();
+        
+        //assert that the output was as expected
+        $this->assertContains( '<meta property="fb:use_automatic_ad_placement" content="true">', $output );
     }
     
     
