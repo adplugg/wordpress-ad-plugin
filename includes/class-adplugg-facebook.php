5a9a3d85f080976b81bf988f6323a52cd07d5c05
Collin Krawll <collin.krawll@thirdset.com>
2019-01-08 09:17:36 -0800

    Linted the AdPlugg_Facebook class to meet all WP coding standards.

--- includes/class-adplugg-facebook.php
+++ includes/class-adplugg-facebook.php
@@ -1,17 +1,20 @@
 <?php
-
 /**
- * AdPlugg Facebook class.
  * The AdPlugg Facebook class controls AdPlugg's Facebook intragration. This
  * class is used by both the frontend and the admin.
  *
  * @package AdPlugg
  * @since 1.3.0
  */
+
+/**
+ * AdPlugg Facebook class.
+ */
 class AdPlugg_Facebook {
 
 	/**
 	 * Singleton instance.
+	 *
 	 * @var AdPlugg_Facebook
 	 */
 	private static $instance;
@@ -19,7 +22,7 @@ class AdPlugg_Facebook {
 	/**
 	 * Constructor, constructs the class and registers filters and actions.
 	 */
-	function __construct() {
+	public function __construct() {
 		add_action( 'widgets_init', array( &$this, 'facebook_instant_articles_header_widget_area_init' ) );
 		add_action( 'instant_articles_compat_registry_ads', array( $this, 'add_to_registry' ), 10, 1 );
 	}
@@ -27,27 +30,30 @@ class AdPlugg_Facebook {
 	/**
 	 * Add the Facebook Instant Articles Header widget area.
 	 */
-	function facebook_instant_articles_header_widget_area_init() {
+	public function facebook_instant_articles_header_widget_area_init() {
 		if ( self::is_ia_automatic_placement_enabled() ) {
-			register_sidebar( array(
-					'name'			=> 'Facebook Instant Articles Ads',
-					'id'			=> 'facebook_ia_header_ads',
-					'description'	=> 'Drag the AdPlugg Widget here to have AdPlugg Ads automatically included in your Facebook Instant Articles.',
-			) );
+			register_sidebar(
+				array(
+					'name'        => 'Facebook Instant Articles Ads',
+					'id'          => 'facebook_ia_header_ads',
+					'description' => 'Drag the AdPlugg Widget here to have AdPlugg Ads automatically included in your Facebook Instant Articles.',
+				)
+			);
 		}
 	}
 
 	/**
 	 * Function that looks to see if Facebook Instant Articles automatic ad
 	 * placement is enabled.
+	 *
 	 * @return boolean Returns true if an Facebook Instant Articles automatic ad
 	 * placement is enabled, otherwise returns false.
 	 */
-	static function is_ia_automatic_placement_enabled() {
+	public static function is_ia_automatic_placement_enabled() {
 		$options = get_option( ADPLUGG_FACEBOOK_OPTIONS_NAME, array() );
 		$enabled = false;
 		if ( ! empty( $options['ia_enable_automatic_placement'] ) ) {
-			$enabled = ($options['ia_enable_automatic_placement'] == 1) ? true : false;
+			$enabled = ( 1 === $options['ia_enable_automatic_placement'] ) ? true : false;
 		}
 
 		return $enabled;
@@ -56,14 +62,15 @@ class AdPlugg_Facebook {
 	/**
 	 * Function that looks to see if the temporary Use Legacy adplugg.com
 	 * Endpoint setting is turned on.
+	 *
 	 * @return boolean Returns true if the temporary Use Legacy adplugg.com
 	 * Endpoint setting is turned on, otherwise returns false.
 	 */
-	static function temp_use_legacy_adplugg_com_endpoint() {
+	public static function temp_use_legacy_adplugg_com_endpoint() {
 		$options = get_option( ADPLUGG_FACEBOOK_OPTIONS_NAME, array() );
 		$enabled = false;
 		if ( ! empty( $options['temp_use_legacy_adplugg_com_endpoint'] ) ) {
-			$enabled = ($options['temp_use_legacy_adplugg_com_endpoint'] == 1) ? true : false;
+			$enabled = ( 1 === $options['temp_use_legacy_adplugg_com_endpoint'] ) ? true : false;
 		}
 
 		return $enabled;
@@ -72,14 +79,15 @@ class AdPlugg_Facebook {
 	/**
 	 * Function that looks to see if the temporary Allow Legacy adplugg.com
 	 * Endpoint setting is turned on.
+	 *
 	 * @return boolean Returns true if the temporary Allow Legacy adplugg.com
 	 * Endpoint setting is turned on, otherwise returns false.
 	 */
-	static function temp_allow_legacy_adplugg_com_endpoint() {
+	public static function temp_allow_legacy_adplugg_com_endpoint() {
 		$options = get_option( ADPLUGG_FACEBOOK_OPTIONS_NAME, array() );
 		$enabled = false;
 		if ( ! empty( $options['temp_allow_legacy_adplugg_com_endpoint'] ) ) {
-			$enabled = ($options['temp_allow_legacy_adplugg_com_endpoint'] == 1) ? true : false;
+			$enabled = ( 1 === $options['temp_allow_legacy_adplugg_com_endpoint'] ) ? true : false;
 		}
 
 		return $enabled;
@@ -91,18 +99,19 @@ class AdPlugg_Facebook {
 	 *
 	 * @param array $registry Reference param. The registry where it will be stored.
 	 */
-	function add_to_registry( &$registry ) {
+	public function add_to_registry( &$registry ) {
 
 		$display_name = 'AdPlugg';
-		$identifier = 'adplugg';
+		$identifier   = 'adplugg';
 
 		$registry[ $identifier ] = array(
-				'name' => $display_name,
+			'name' => $display_name,
 		);
 	}
 
 	/**
 	 * Gets the singleton instance.
+	 *
 	 * @return \AdPlugg_Facebook Returns the singleton instance of this class.
 	 */
 	public static function get_instance() {
