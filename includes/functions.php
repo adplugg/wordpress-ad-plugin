aac64fda1a2abdcb8d64a2ba546a84c758dd8dc1
Collin Krawll <collin.krawll@thirdset.com>
2019-01-08 09:22:14 -0800

    Linted the functions.php file to meet all WP coding standards.

--- includes/functions.php
+++ includes/functions.php
@@ -1,22 +1,24 @@
 <?php
 /**
  * Miscellaneous functions used by the plugin.
+ *
  * @package AdPlugg
  * @since 1.0
  */
 
 /**
  * Function that looks to see if the AdPlugg Widget has been activated.
- * @return boolean Returns true if the widget is activated, otherwise
- * returns false.
+ *
+ * @return boolean Returns true if the widget is activated, otherwise, returns
+ * false.
  */
 function adplugg_is_widget_active() {
 	$widget_options = get_option( ADPLUGG_WIDGET_OPTIONS_NAME );
-	
-	/* is_active_widget doesn't work if the plugin is deactivated and then reactivated.
-	 * if the widget is active it will have more than one option, so this works.
-	 */
-	if ( ( ! is_null( $widget_options ) ) && ( sizeof( $widget_options ) > 1 ) ) {
+
+	// is_active_widget doesn't work if the plugin is deactivated and then
+	// reactivated. If the widget is active, it will have more than one option,
+	// so this works.
+	if ( ( ! is_null( $widget_options ) ) && ( count( $widget_options ) > 1 ) ) {
 		return true;
 	} else {
 		return false;
