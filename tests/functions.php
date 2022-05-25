ec100a635d0b21d82507790737883f51afec96eb
Collin Krawll <collin.krawll@thirdset.com>
2022-05-25 14:21:50 -0700

    Updated the get_function_names test helper function to accomodate recent changes to the  variable.

--- tests/functions.php
+++ tests/functions.php
@@ -8,15 +8,28 @@
 /**
  * Given a base registry array entry, this private function will return all
  * function names within that array.
- * @param array $reg_array_1 The base registry array that you want to search
- * through.
+ *
+ * @param array $reg The base registry that you want to search through.
  * @return array An array of the function names that were found.
  */
-function get_function_names( $reg_array_1 ) {
+function get_function_names( $reg ) {
 	$function_names = array();
-	foreach ( $reg_array_1 as $reg_array_2 ) {
-		foreach ( $reg_array_2 as $reg_array_3 ) {
-			$function_names[] = $reg_array_3['function'][1];
+
+	$callbacks = $reg->callbacks;
+
+	// Note: the variable names here could probably be improved.
+	foreach ( $callbacks as $callback ) {
+		foreach ( $callback as $function ) {
+			$func = $function['function'];
+			if (is_array($func)) {
+				//$class = (is_object($func[0])) ? get_class($func[0]) : $func[0];
+				$method = $func[1];
+				//$function_name = $class . '::' . $method;
+				$function_name = $method;
+			} else {
+				$function_name = $func;
+			}
+			$function_names[] = $function_name;
 		}
 	}
 
